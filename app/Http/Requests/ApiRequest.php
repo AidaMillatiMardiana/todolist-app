4facca4eef0ca27a4cdaf88ba71ba9f129d57317
AidaMillatiMardiana <aidadiana2702@gmail.com>
2022-04-25 23:37:59 +0700

    Jobsheet 11 Praktikum 2

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -11,17 +11,28 @@ use Illuminate\Http\Response;
 abstract class ApiRequest extends FormRequest
 {
     use ApiResponse;
-
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
     abstract public function rules();
 
-    protected function failedValidation(Validator $validator){
+    protected function failedValidation(Validator $validator)
+    {
         throw new HttpResponseException($this->apiError(
             $validator->errors(),
             Response::HTTP_UNPROCESSABLE_ENTITY,
         ));
     }
 
-    protected function failedAuthorization(){
+    protected function failedAuthorization()
+    {
         throw new HttpResponseException($this->apiError(
             null,
             Response::HTTP_UNAUTHORIZED
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
+});
